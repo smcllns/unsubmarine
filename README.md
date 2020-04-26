d018193b0549a47329de2679282302da58c17be8
Sam Collins <oscollins@gmail.com>
2020-04-25 18:09:24 -0700

    Part way to mvp v2 - this loops through emails and grabs unsub links, but doesnt provide a review and let user take action on them yet

--- README.md
+++ README.md
@@ -2,36 +2,6 @@
 
 Hacking an auto-unsubscribe into gmail
 
-## Problem Statement
+## More Info
 
-It's less visible day to day - but if I go off the grid for a week, when I open my inbox there's a lot of email I really never wanted to receive. What I'd like is for a robot to go through my inbox and automatically unsubscribe me from any senders I don't want to hear from.
-
-## Alternative Solutions (that aren't working for me)
-
-1. Filters (either manual or gmail's automated filtering). Very helpful, but I still need to parse through lists to look for misses and keeping filters up to date and accurate is becoming a lot of work. Simply, it would be better if the noise was never there.
-
-1. Gmail's "auto-unsubscribe". Overall, I think gmail team could do a _lot more_ here. I don't know why they don't - maybe it's a perverse conflict of interest (maybe automatically cleaning up my email reduces core metrics visitation/engagement), or maybe this is just a lower priority area vs areas of growth for gmail.
-
-1. Manually unsubscribe to each email as I receive it. As per the problem statement, this isn't working for me. I'll spare the psychological analysis for why this might be.
-
-1. Use clever email aliases (e.g. myemail+spam@gmail.com). At least two reasons this doesn't work for me - a) at signup time, I don't know if they're going to be spammy/forget to use my clever email suffix b) sometimes not an option if I _Sign-in with X_ and my email address is automatically shared as is.
-
-1. Browser Extensions/Apps that do this. I can only find unroll.me - which is opaque in how it works and mines my inbox for business intelligence on behalf of Rakuten (you can opt-out), but just freaks me out a bit. I'd happily pay \$10 for a spring clean instead.
-
-## How should this work?
-
-This is a work in progress. To start with, I'd like to achieve the following:
-
-- Create an easy workflow to keep my inbox clean - e.g. a periodic spring clean of my inbox every X months.
-- Give me a way to batch unsubscribe to multiple senders in one-click.
-- Let me approve so I don't unsubscribe from something I didn't want to.
-- Don't require that I enter clever email addresses at sign-up time.
-
-## How to try out this prototype:
-
-1. Go to gmail in your browser on a computer
-2. Add index.js to the DOM (open the JS console and copy paste index.js into it)
-3. Prepare a list of emails to loop through. I suggest starting with [mail.google.com/search/unsubscribe](mail.google.com/search/unsubscribe) but you can use any list of messages
-4. Click the first message in the message.
-5. At this point you should be looking at an email message, with Prev/Next buttons at the top of the message that will take you to the next email in the list.
-6. In the browser console start the script by entering: `unsubmarine()`
+https://smcllns.com/memos/unsubmarine
--- index.js
+++ index.js
@@ -1,62 +1,127 @@
 function unsubmarine(nProvided) {
+  // All let bc pasting const into console can only be done once
   let i,
     n,
     delay = 1000,
     results;
 
-  function __reset() {
+  function reset() {
     i = 0;
     n = 0;
-    results = { scraped: [], actioned: [] };
+    results = {
+      scraped: [],
+      actioned: [],
+      toActionMap: new WeakMap(),
+      toActionArr: [],
+    };
   }
 
-  function __scrape() {
-    let possibleUnsubLinks = Array.from(
-      document.querySelectorAll('[role="listitem"] a')
-    ).filter((a) => /unsubscribe/i.test(a?.textContent));
-    let possibleUnsubBtn = document.querySelector("[idlink]");
+  function isListView() {
+    // There are visible 'rows'
+    return (
+      Array.from(document.querySelectorAll("[role=main] [role=row]")).filter(
+        (node) => node.offsetParent
+      ).length > 0
+    );
+  }
+
+  function isMessageView() {
+    // There is a visible 'listitem'
+    return (
+      Array.from(
+        document.querySelectorAll("[role=main] [role=listitem]")
+      ).filter((node) => node.offsetParent).length > 0
+    );
+  }
+
+  function scrapeList() {
+    let possibleListItems = Array.from(
+      document.querySelectorAll("[role=main] [role=row]")
+    ).filter((node) => node.offsetParent);
+
+    let firstListItem = possibleListItems[0];
+
+    let listLength = possibleListItems.length;
+
     let possibleNextBtns = true
       ? document.querySelectorAll('[data-tooltip="Older"]')
       : document.querySelectorAll('[data-tooltip="Newer"]');
 
-    let sender = document.querySelectorAll("span .go")[0]?.textContent;
-    let subject = document.querySelectorAll("[data-thread-perm-id]")[0]
-      ?.textContent;
-    let when = document.querySelectorAll('[alt*=":"][title*=":"]')[0]
-      ?.textContent;
-    let url = window.location.href;
+    let nextBtn = Array.from(possibleNextBtns).filter(
+      (node) => node.offsetParent
+    )[0];
+
+    return {
+      length: listLength,
+      firstItem: firstListItem,
+    };
+  }
+
+  function scrapeMessage() {
+    let possibleUnsubLinks = Array.from(
+      document.querySelectorAll('[role="listitem"] a')
+    ).filter((a) => /unsubscribe/i.test(a?.textContent));
+
+    let unsubLink =
+      possibleUnsubLinks.length > 0 ? possibleUnsubLinks[0] : false;
+
+    let possibleUnsubBtn = document.querySelector("[idlink]");
+
     let unsubBtn =
       possibleUnsubBtn?.textContent?.toLowerCase() === "unsubscribe"
         ? possibleUnsubBtn
         : false;
-    let unsubLink =
-      possibleUnsubLinks.length > 0 ? possibleUnsubLinks.pop() : false;
-    let nextBtn = Array.from(possibleNextBtns)
-      .filter((n) => n.offsetParent)
-      .pop();
+
+    let possibleNextBtns = true
+      ? document.querySelectorAll('[data-tooltip="Older"]')
+      : document.querySelectorAll('[data-tooltip="Newer"]');
+
+    let nextBtn = Array.from(possibleNextBtns).filter(
+      (node) => node.offsetParent
+    )[0];
+
+    let sender = document.querySelectorAll("span .go")[0]?.textContent;
+
+    let subject = document.querySelectorAll("[data-thread-perm-id]")[0]
+      ?.textContent;
+
+    let when = document.querySelectorAll('[alt*=":"][title*=":"]')[0]
+      ?.textContent;
+
+    let url = window.location.href;
 
     return {
       sender,
       subject,
       when,
-      unsubLink,
+      unsubLink: unsubLink.href,
       unsubBtn,
       nextBtn,
       url,
     };
   }
 
-  function __run(prevM) {
-    let m = __scrape();
-    if (i >= n) return __printResults(results, { i, n, m, msg: "i>=n" });
+  function run(prevM) {
+    if (isListView()) {
+      let l = scrapeList();
+      l.firstItem.click();
+      setTimeout(() => {
+        run();
+      }, delay * 5);
+    }
+
+    let m = scrapeMessage();
+    if (i >= n) return printResults(results, { i, n, m, msg: "i>=n" });
     if (m?.url === prevM?.url)
-      return __printResults(results, { i, n, m, msg: "same url" });
+      return printResults(results, { i, n, m, msg: "same url" });
 
     results.scraped.push(m);
 
     if (m.unsubLink) {
-      m.unsubLink.click();
-      results.actioned.push(m);
+      // m.unsubLink.click();
+      // results.actioned.push(m);
+      if (!results.toActionMap.has(m.unsubLink)) results.toActionArr.push(m);
+      results.toActionMap.set(m, m.unsubLink);
     }
 
     if (m.nextBtn) {
@@ -64,26 +129,27 @@ function unsubmarine(nProvided) {
       i++;
     }
 
-    console.log("tick", { i, n, m, r: results.actioned });
+    console.log("tick", { i, n, m, r: results.toActionArr });
     setTimeout(() => {
-      __run(m);
+      run(m);
     }, delay);
   }
 
-  function __printResults(results, debug) {
+  function printResults(results, debug) {
     console.log(`Debug:`, debug);
-    console.group(`${results.actioned.length} Results Actioned:`);
-    results.actioned?.map((r) =>
+    console.group(`${results.toActionArr.length} Results To Action:`);
+    results.toActionArr?.map((r) =>
       console.log(
-        `${r.when} - ${r.sender?.replace(/[\<\>]/g, "")} - ${r.subject}`
+        `${r.when} - ${r.sender?.replace(/[\<\>]/g, "")} - ${r.subject}: ${
+          r.unsubLink
+        }`
       )
     );
-    console.log();
     console.groupEnd();
   }
 
-  (function __start(nProvided) {
-    __reset();
+  (function start(nProvided) {
+    reset();
     n =
       nProvided ||
       prompt(
@@ -95,6 +161,6 @@ function unsubmarine(nProvided) {
     n = parseInt(n, 10);
     if (!Number.isInteger(n))
       return alert("Number must be an integer greater than zero");
-    __run();
+    run();
   })(nProvided);
 }
