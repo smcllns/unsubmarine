2f8262c481844a778df5ad3dbefd4ce32d7735a4
Sam Collins <oscollins@gmail.com>
2020-05-02 18:26:54 -0700

    Renders a really basic UI after running with redom

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ Hacking an auto-unsubscribe into gmail
 ## More Info
 
 https://smcllns.com/memos/unsubmarine
+
+### Libraries/Dependencies
+
+For now, to avoid a big node_modules folder, just keeping track of src for any external libraries here:
+
+- https://unpkg.com/crx-hotreload // Hot reload for developing Chrome extension [https://github.com/xpl/crx-hotreload](https://github.com/xpl/crx-hotreload)
+- https://unpkg.com/redom@latest/dist/redom.es.min.js // Re:DOM for faster vanilla JS DOM manipulation [https://github.com/redom](https://github.com/redom)
--- content/handleResults.js
+++ content/handleResults.js
@@ -1,8 +1,43 @@
+import { el, mount } from "../lib/redom.es.min.js";
+
 export default function handleResults(results, debugObj) {
-  consoleLogResults(results, debugObj);
+  consoleDump(results, debugObj);
+  renderDom(results, debugObj);
 }
 
-function consoleLogResults(results, debug) {
+function renderDom(results, debug) {
+  const hello = el(
+    "div#test",
+    {
+      style: {
+        color: "white",
+        backgroundColor: "red",
+        position: "absolute",
+        zIndex: "999",
+        top: "0",
+        left: "0",
+        right: "0",
+        bottom: "0",
+        padding: "4rem",
+        alignItems: "center",
+        justifyContent: "center",
+        wordBreak: "break-all",
+        flexDirection: "column",
+        overflowY: "auto",
+      },
+    },
+    el("h1", "Hello from Unsubmarine."),
+    el("p", "Here are the results:"),
+    el(
+      "code",
+      { style: { whiteSpace: "pre-wrap" } },
+      JSON.stringify(results, null, 1)
+    )
+  );
+  mount(document.body, hello);
+}
+
+function consoleDump(results, debug) {
   console.log(`Debug:`, debug);
   console.group(`${results.toActionArr.length} Results To Action:`);
   results.toActionArr?.map((r) =>
